68df8e58ccaf77899fc06f62aed953502d82e793
yuri-1984 <yuri.hiraoka@rakus-partners.co.jp>
2020-11-19 14:00:08 +0900

    日本語変更とヘッダー変更

---
+++ learning-report/resources/views/home.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-body">
+                    <!-- componentにわけてここにのせれば、一枚ですむかも -->
+                    <div class="pb-3">
+                        <button type="button" class="btn btn-primary btn-lg btn-block">教員</button>
+                    </div>
+                    <div class="pb-3">
+                        <button type="button" class="btn btn-primary btn-lg btn-block">生徒</button>
+                    </div>
+                    <div class="pb-3">
+                        <button type="button" class="btn btn-primary btn-lg btn-block">科目の設定</button>
+                    </div>
+                    <div class="pb-3">
+                        <button type="button" class="btn btn-primary btn-lg btn-block">担当教科・担当クラスの登録</button>
+                    </div>
+                    <div class="news">
+                        <h3>新着情報</h3>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item d-flex flex-wrap">
+                                <div class="d-flex col-12 col-md-auto">
+                                    <div class="news-date">7月14日</div>
+                                    <div class="news-category">お知らせ</div>
+                                </div>
+                                <div class="news-subject col-12 col-md-9">〇〇〇</div>
+                            </li>
+                            <li class="list-group-item d-flex flex-wrap">
+                                <div class="d-flex col-12 col-md-auto">
+                                    <div class="news-date ">7月14日</div>
+                                    <div class="news-category">イベント情報</div>
+                                </div>
+                                <div class="news-subject col-12 col-md-9">
+                                    本日のイベントは雨天のため、中止いたします。本日のイベントは雨天のため、中止いたします。本日のイベントは雨天のため、中止いたします。本日のイベントは雨天のため、中止いたします。
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        @endsection
